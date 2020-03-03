87ab23c18666a521e86d9bccf4e0e4b7acf683ed
Luka Radovanovic <lukadlm97@gmail.com>
2020-03-03 16:23:12 +0100

    Working with components and their props

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -6,8 +6,16 @@ import Welcome from './components/Welcome'
 function App() {
   return (
     <div className="App">
-     <Greet />
-     <Welcome />
+     <Greet name='Luka' heroName='Student'>
+       <p>This is children props</p>
+     </Greet>
+     <Greet name='Clark' heroName='Superman'>
+       <button>Action</button>
+       </Greet>
+     <Greet name='Diana' heroName='Superwomen'/>
+     <Welcome name='Bruce' heroName='Betman' />
+     <Welcome name='Clark' heroName='Superman'/>
+     <Welcome name='Luka' heroName='Goman'/>
     </div>
   );
 }
