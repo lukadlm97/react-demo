218f901e1551a522b26fdcffdf74efbc36bfff35
Luka Radovanovic <lukadlm97@gmail.com>
2020-03-04 18:47:59 +0100

    Purpose of setState and purpose of prevState in methods

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Message from './components/Message'
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-    <Message />
+      <Counter/>
+    {/*<Message /> */}
     {/* <Greet name='Luka' heroName='Student'>
        <p>This is children props</p>
      </Greet>
