0d12521c97cdac4ef9f5930303affa26f7cfef1c
Luka Radovanovic <lukadlm97@gmail.com>
2020-03-07 22:10:52 +0100

    Event handling using components and functions

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -5,13 +5,19 @@ import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Message from './components/Message'
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 function App() {
   return (
     <div className="App">
+      <FunctionClick />
+      <ClassClick />
+     {/* 
       <Welcome name='Luka' heroName='Student' />
       <Greet name='Luka' heroName='Superman' />
-     {/* <Counter/>*/}
+     <Counter/>
+     */}
     {/*<Message /> */}
     {/* <Greet name='Luka' heroName='Student'>
        <p>This is children props</p>
