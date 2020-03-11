47b69278c645eafc87b967d9b0210942010b07be
Luka Radovanovic <lukadlm97@gmail.com>
2020-03-11 21:06:31 +0100

    Introduction to working with forms in react

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -15,17 +15,19 @@ import Stylesheet from './components/Stylesheet';
 import Inline from './components/Inline';
 import './appStyles.css';
 import styles from './appStyles.module.css';
-
+import Form from './components/Form';
 function App() {
   return (
     <div className="App">
-      <h2 className='error'>Error</h2>
-      <h2 className={styles.success}>Success</h2>
-      <Inline />
+
+      <Form />
 
 
      {/* 
       <Stylesheet primary={true} />
+      <h2 className='error'>Error</h2>
+      <h2 className={styles.success}>Success</h2>
+      <Inline />
       <NameList />
       <UserGreeting />
       <ParentComponent />
